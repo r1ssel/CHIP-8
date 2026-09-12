911147194821c4499bdbff859904dc4cd4996a67
r1ssel <retina.storee@gmail.com>
2026-09-12 13:57:51 +0300

    add chip8.h

---
+++ src/chip8.h
@@ -0,0 +1,32 @@
+
+#include <stdint.h>
+
+class chip8 {
+    public:
+        chip8();
+        ~chip8();
+
+        bool drawFlag;
+
+        void emulateCycle();
+        void debugRender();
+        bool loadApplication(const char * filename);
+
+        uint8_t gfx[54 * 32];
+        uint8_t key[16];
+
+    private:
+        uint16_t pc;
+        uint16_t opcode;
+        uint16_t I;
+        uint16_t sp;
+
+        uint8_t V[16];
+        uint16_t stack[16];
+        uint8_t memory[4096];
+
+        uint8_t delay_timer;
+        uint8_t sound_timer;
+
+        void init();
+};
