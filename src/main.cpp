e784c30809a7862892f6509abe86c02e67d5375d
r1ssel <retina.storee@gmail.com>
2026-09-12 17:50:04 +0300

    add OpenGL glut/freeglut

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include "chip8.h"
+#include <GL/glut.h>
 
 chip8 myChip;
 
-int main() {
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
+
+int main(int argc, char** argv) {
     static bool init = true;
     if (init){
         init = false;
         printf("Init...\r\n");
         printf("Init completed\r\n");
     }
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Chip8");
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     myChip.emulateCycle();
     
